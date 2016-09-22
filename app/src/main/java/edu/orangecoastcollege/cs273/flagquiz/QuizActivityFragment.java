7e58611bbc873314862d6640304becb22b3cf146
Ethan <ethan.quach.10@gmail.com>
2016-09-22 16:53:59 -0700

    Coded stuff for QuizActivity (finished) and QuizActivityFragment (working on it)

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivityFragment.java
@@ -1,15 +1,45 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
-import android.support.v4.app.Fragment;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class QuizActivityFragment extends Fragment {
+    // String used when logging error messages
+    private static final String TAG = "FlagQuiz Activity";
+
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList; // filenames for country flags
+    private List<String> quizCountriesList; // countries in current quiz
+    private Set<String> regionsSet; // world regions in current quiz
+    private String correctAnswer; // correct country for current flag
+    private int totalGuesses, correctAnswers; // only valid for current quiz;
+    private int guessRows; // number of rows displaying the guess Buttons
+    private SecureRandom random; // used to randomize the quiz
+    private Handler handler; // used to delay loading next flag
+
+    private TextView questionNumberTextView; // shows current question number
+    private ImageView flagImageView; // displays a flag
+    private LinearLayout[] guessLinearLayouts; // this is for the rows of answer Buttons
+    private TextView answerTextView; // displays correct answer
 
     public QuizActivityFragment() {
     }
@@ -17,6 +47,57 @@ public class QuizActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+
+        // hooking up GUI elements to the components in corresponding XML file
+        questionNumberTextView = (TextView) view.findViewById(R.id.questionNumberTextView);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+        guessLinearLayouts = new LinearLayout[4];
+        guessLinearLayouts[0] =
+                (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+        guessLinearLayouts[1] =
+                (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+        guessLinearLayouts[2] =
+                (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+        guessLinearLayouts[3] =
+                (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+
+        // creating listeners for the guess Buttons
+        for (LinearLayout row : guessLinearLayouts) {
+            for (int column = 0; column < row.getChildCount(); column++) {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
+
+        // setting questionNumberTextView's text
+        questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+
+        return view;
+    }
+
+    public void updateGuessRows(SharedPreferences sharedPreferences) {
+        String choices = sharedPreferences.getString(QuizActivity.CHOICES, null);
+        guessRows = Integer.parseInt(choices) / 2;
+
+        for (LinearLayout l : guessLinearLayouts)
+            layout.setVisibility(View.GONE);
+
+        for (int row = 0; row < guessRows; row++)
+            guessLinearLayouts[row].setVisibility(View.VISIBLE);
+    }
+
+    public void updateRegions(SharedPreferences sharedPreferences) {
+        regionsSet = sharedPreferences.getStringSet(QuizActivity.REGIONS, null);
+    }
+
+    public void resetQuiz() {
+        AssetManager assets = getActivity().getAssets();
+        fileNameList.clear();
     }
 }
