9639977dae2b320b6c402857727804d4c5b93ce2
Ethan <ethan.quach.10@gmail.com>
2016-09-22 16:07:45 -0700

    finished code for flags to determine what device and what orientation the user is operating

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivity.java
@@ -1,15 +1,21 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+    // keys for reading data from SharedPreferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,21 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // sets default values in the app's SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        // new listener for SharedPreferences changes
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false;
+
+        if (phoneDevice) setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     @Override
